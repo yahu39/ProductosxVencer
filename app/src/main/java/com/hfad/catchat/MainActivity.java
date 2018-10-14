182549e64b90d6fc5c283e2fa93ed6169d5d21b9
Pablo <yahu39pc@gmail.com>
2018-10-14 07:15:43 -0500

    Initial commit

---
+++ app/src/main/java/com/hfad/catchat/MainActivity.java
@@ -0,0 +1,152 @@
+package com.hfad.catchat;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.support.v4.app.FragmentManager;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener, DatePickerDialog.OnDateSetListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //creando el toogle-->sandwich icon
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,
+                drawer,
+                toolbar,
+                R.string.nav_open_drawer,
+                R.string.nav_close_drawer);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        //fin de la creacion del toogle
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //cargando el fragment principal
+        Fragment fragment = new InboxFragment();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.add(R.id.content_frame, fragment);
+        ft.commit();
+    }
+        //cargando el datepicker
+        /**
+         * This callback method, call DatePickerFragment class,
+         * DatePickerFragment class returns calendar view.
+         * @param view
+         */
+
+       public void datePicker(View view){
+
+            DatePickerFragment fragment = new DatePickerFragment();
+
+           fragment.show(getFragmentManager(),"d");
+
+          //  fragment.show(getSupportFragmentManager(),&quot;date&quot;);
+        }
+
+
+        /**
+         * To set date on TextView
+         * @param calendar
+         */
+        private void setDate(final Calendar calendar) {
+            final DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM);
+
+            ((TextView) findViewById(R.id.etxt_fecha))
+                    .setText(dateFormat.format(calendar.getTime()));
+
+        }
+
+        /**
+         * To receive a callback when the user sets the date.
+         * @param view
+         * @param year
+         * @param month
+         * @param day
+         */
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            Calendar cal = new GregorianCalendar(year, month, day);
+            setDate(cal);
+        }
+
+
+    //fin del datepicker
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        Fragment fragment = null;
+        Intent intent = null;
+
+        switch(id) {
+            case R.id.nav_drafts:
+                fragment = new DraftsFragment();
+                break;
+            case R.id.nav_sent:
+                fragment = new SentItemsFragment();
+                break;
+            case R.id.nav_trash:
+                fragment = new TrashFragment();
+                break;
+            case R.id.nav_help:
+                intent = new Intent(this, HelpActivity.class);
+                break;
+            case R.id.nav_feedback:
+                intent = new Intent(this, FeedbackActivity.class);
+                break;
+            default:
+                fragment = new InboxFragment();
+        }
+
+        if (fragment != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.content_frame, fragment);
+            ft.commit();
+        } else {
+            startActivity(intent);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+}
